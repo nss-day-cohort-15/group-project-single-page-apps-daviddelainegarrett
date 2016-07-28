80e6ea30eadab7857539649e9ec5b73f0cbccc3e
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-28 14:07:19 -0500

    Deleted event listeners file

--- eventListeners.js
+++
@@ -1,8 +0,0 @@
-// document.querySelector("body").addEventListener("click", function(event) {
-
-//    if (event.target.className === "deleteBtn") {
-//     console.log(event.target.parentElement);
-//     userMessage.deleteMessages(Messages.getArray())
-//   }
-// });
-
